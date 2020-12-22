a76b95bee5d54e79eaecc7e76efb452bf563b0fd
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-12-22 22:43:47 +0900

    if文

--- main.php
+++ main.php
@@ -1,19 +1,11 @@
 <?php
 
-$a = 'hallo';
-$b = 10;
-$c = -1.3;
-$d = null;
-$e = true;
+$score = 85;
 
-var_dump($a);
-var_dump($b);
-var_dump($c);
-var_dump($d);
-var_dump($e);
-
-$a = (float)10;
-$b = (string)1.3;
-
-var_dump($a);
-var_dump($b);
+if($score >= 80) {
+  echo 'Great' . PHP_EOL;
+} elseif ($score >= 60) {
+  echo 'good' . PHP_EOL;
+} else {
+  echo 'OK' . PHP_EOL;
+}
