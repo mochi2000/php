b913fee7f786876c87370ad5061d5f9edddf62ce
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-12-28 21:44:53 +0900

    while文

--- main.php
+++ main.php
@@ -1,6 +1,14 @@
 <?php
 
-for ($i = 1; $i <= 5; $i++) {
-  echo "$i - Hello" . PHP_EOL;
-}
+$hp = 100;
 
+while($hp > 0) {
+  echo "Your HP: $hp" . PHP_EOL;
+  $hp -= 15;
+}
+//条件を書かないと無限ループになってしまう
+
+// do {
+//   echo "Your HP: $hp" . PHP_EOL;
+//   $hp -= 15;
+// } while ($hp > 0);
