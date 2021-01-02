781992eeae770ce5451829e674805be708a6c233
mochi2000 <umaibou2000jp@yahoo.co.jp>
2021-01-02 22:33:18 +0900

    変数のスコープ

--- main.php
+++ main.php
@@ -1,8 +1,11 @@
 <?php
 
+$rate = 1.1; //グローバル
+
 function sum($a, $b, $c) 
 {
-  return $a+$b+$c . PHP_EOL;
+  $rate = 1.08; //ローカル
+  return $a+$b+$c * $rate. PHP_EOL;
 }
 
 echo sum(100,200,300) + sum(200,400,400) .PHP_EOL;
