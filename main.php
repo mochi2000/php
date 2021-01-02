c04eaacd56b226a7f3658024feea52eddc832b1e
mochi2000 <umaibou2000jp@yahoo.co.jp>
2021-01-02 22:39:42 +0900

    計算を修正

--- main.php
+++ main.php
@@ -5,7 +5,7 @@ $rate = 1.1; //グローバル
 function sum($a, $b, $c) 
 {
   $rate = 1.08; //ローカル
-  return $a+$b+$c * $rate. PHP_EOL;
+  return ($a+$b+$c) * $rate . PHP_EOL;
 }
 
 echo sum(100,200,300) + sum(200,400,400) .PHP_EOL;
